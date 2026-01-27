64e6c3aa0e52d19e27fff386993766d49fbd9639
Dominique Klevinghaus <adk@codekommando.de>
2026-01-27 21:21:28 +0100

    fix cv

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ const IndexPage = () => {
                 University of Warwick’s Centre for the Study of Globalisation and Regionalisation.
               </p>
               <p>
-                You can find a copy of my CV <a href="cv/CV Florence Dafe_web1024.pdf">here</a>.
+                You can find a copy of my CV <a href="cv/cv-florence-dafe.pdf">here</a>.
               </p>
             </div>
           </div>
